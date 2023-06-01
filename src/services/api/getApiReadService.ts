978ccb509785376f68bc8ea9b0ff268c0bc99e6c
dsinghvi <deep@usebirch.com>
2023-06-01 13:22:14 -0400

    add environments for backcompat

--- src/services/api/getApiReadService.ts
+++ src/services/api/getApiReadService.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-import * as FernRegistryApiRead from "../../generated/api/resources/api/resources/v1/resources/read";
+import * as FernRegistryApiDb from "../../generated/api/resources/api/resources/v1/resources/db";
 import { ApiDoesNotExistError } from "../../generated/api/resources/api/resources/v1/resources/read/errors/ApiDoesNotExistError";
 import { ReadService } from "../../generated/api/resources/api/resources/v1/resources/read/service/ReadService";
 import * as FernSerializers from "../../generated/serialization";
 import { readBuffer } from "../../serdeUtils";
+import { transformApiDefinitionForReading } from "./dbToReadConversion/transformDbApiDefinitionToRead";
 
 export function getReadApiService(prisma: PrismaClient): ReadService {
     return new ReadService({
@@ -17,12 +18,12 @@ export function getReadApiService(prisma: PrismaClient): ReadService {
                 throw new ApiDoesNotExistError();
             }
             const parsedApiDefinition = await convertDbApiDefinitionToRead(apiDefinition.definition);
-            return res.send(parsedApiDefinition);
+            return res.send(transformApiDefinitionForReading(parsedApiDefinition));
         },
     });
 }
 
-export async function convertDbApiDefinitionToRead(buffer: Buffer): Promise<FernRegistryApiRead.ApiDefinition> {
+export async function convertDbApiDefinitionToRead(buffer: Buffer): Promise<FernRegistryApiDb.DbApiDefinition> {
     const apiDefinitionJson = readBuffer(buffer);
-    return await FernSerializers.api.v1.read.ApiDefinition.parseOrThrow(apiDefinitionJson);
+    return await FernSerializers.api.v1.db.DbApiDefinition.parseOrThrow(apiDefinitionJson);
 }
